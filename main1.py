de94aae458a52e7853c88fa87c115ac87cfc7c58
Chance03-git <Chancefoster05@gmail.com>
2024-11-21 17:24:03 -0500

    Fix test cases and clean up code

--- main1.py
+++ main1.py
@@ -12,3 +12,6 @@ def add(num1, num2):
 
 def test_should_return_four_for_two_and_two():
     assert add(2, 2) == 4
+
+def test_return_the_length_of_string_and_return():
+   assert count("Hello") == 5 
--- main2.py
+++
@@ -1,5 +0,0 @@
-def get_string_length()
-
-
-def test_return_the_length_of_string_and_return():
-   assert get_string_length("Hello") == 5 
