91b02565615cbfac9f4d9b8ac2fd646bf40a3b20
Chance03-git <Chancefoster05@gmail.com>
2024-11-21 10:05:29 -0500

    Documentation

--- main1.py
+++ main1.py
@@ -1,6 +1,14 @@
 def add(num1, num2):
-  
-   pass
-   return num1 + num2
+  """ Adds two numbers and returns the results
+    
+    Args:
+     num1: the first summand
+     num2: the second summand 
+
+     returns:
+      The sum of the two numbers
+     """ 
+  return num1 + num2
+
 def test_should_return_four_for_two_and_two():
     assert add(2, 2) == 4
